59f1e66d5ed1b8bef4bec1b36744277a1347235e
Charles Junjie A. Mempin <cjmempin18@gmail.com>
2025-03-11 12:37:34 +0800

    login validation function

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,35 +1,66 @@
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom'
-import logo from '../assets/mapua_logo.svg'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import logo from '../assets/mapua_logo.svg';
+import { Link } from 'react-router-dom';
+import validation from './LoginValidation';
 
 function Login() {
+  const [values, setValues] = useState({
+    email: '',
+    password: ''
+  })
+
+  const[errors,setErrors] = useState({})
+  
+  const handleInput = (event) => {
+    setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+  }
+
+  const handleSubmit =(event) => {
+    event.preventDefault();
+    setErrors(validation(values));
+
+  }
 const navigate=useNavigate();
 
     return (
       <div className="flex flex-col bg-gray-950 place-content-center ">
         <div className="flex flex-col place-items-center text-center gap-7">
             <img src={logo} className="flex h-98 w-150" />
-            <h2 className="text-white font-serif font-bold text-6xl">MAPUA LIBRARY</h2>
+            <h2 className="text-white">MAPUA LIBRARY</h2>
         </div>
         <div className="flex place-content-center mt-5">
-            <form className="flex flex-col gap-2" action="" >
-                <label htmlFor="email" className="text-white text-2xl">Email</label>
-                <input type="email" 
-                  placeholder="Enter Email" className="bg-gray-300 rounded-full w-80 h-9 px-3"/>
+           
+            <form className="flex flex-col gap-2" action="" onSubmit={handleSubmit}>
+                
+                <div className="mb-3">
+                  <label htmlFor="email" className="text-white">Email</label>
+                  <input type="email" 
+                    placeholder="Enter Email" 
+                    name='email'
+                    onChange={handleInput}
+                    className="form-control bg-gray-300 rounded-full w-80 h-9 px-3"
+                    />
+                  {errors.email && <span className='text-danger'> {errors.email}</span>}
+                </div>
 
-                <label htmlFor="password" className="text-white text-2xl">Password</label>
-                <input type="password" 
-                  placeholder="Enter Password" className="bg-gray-300 rounded-full w-80 h-9 px-3"/>
+                <div className="mb-3">
+                  <label htmlFor="password" className="text-white">Password</label>
+                  <input type="password" 
+                    placeholder="Enter Password" 
+                    name='password'
+                    className="form-control bg-gray-300 rounded-full w-80 h-9 px-3"
+                    onChange={handleInput}
+                    />
+                  {errors.password && <span className='text-danger'> {errors.password}</span>}
+                </div>
 
-                  <div className="flex place-content-center mt-4 gap-2">
-                    <button className="btn btn-success w-100 rounded-full">Sign in</button>
-                  </div>
-                  <div className="flex place-content-center mt-4 gap-2">
-                    <Link to="/Login" className="btn btn-success w-100 rounded-full text-decoration-none">
-                      Log in
+                    <button type='submit' className='btn btn-success w-100 rounded-full'>Log in</button>
+                  
+                    <Link to="/Signup" className="btn btn-success w-100 rounded-full text-decoration-none">
+                      Signup
                     </Link>
-                  </div>
+                  
 
             </form>
         </div>
