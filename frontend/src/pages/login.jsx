0b5656a69c6306be5909eb6892260a0abc2dd918
Charles Junjie A. Mempin <cjmempin18@gmail.com>
2025-03-11 11:36:43 +0800

    changed UI format

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,25 +1,35 @@
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom'
 import logo from '../assets/mapua_logo.svg'
+import { Link } from 'react-router-dom'
 
 function Login() {
 const navigate=useNavigate();
 
     return (
-      <div class="flex flex-col h-screen bg-gray-950 place-content-center">
-        <div class="flex flex-col place-items-center text-center gap-7">
-            <img src={logo} class="flex h-98 w-150" />
-            <h2 class="text-red-600 font-serif font-bold text-6xl">MAPUA LIBRARY</h2>
+      <div className="flex flex-col bg-gray-950 place-content-center ">
+        <div className="flex flex-col place-items-center text-center gap-7">
+            <img src={logo} className="flex h-98 w-150" />
+            <h2 className="text-white font-serif font-bold text-6xl">MAPUA LIBRARY</h2>
         </div>
-        <div class="flex place-content-center mt-5">
-            <form class="flex flex-col gap-2">
-                <label class="text-white text-2xl">Username</label>
-                <input type="text" class="bg-gray-300 rounded-full w-80 h-9 px-3"></input>
-                <label class="text-white text-2xl">Password</label>
-                <input type="password" class="bg-gray-300 rounded-full w-80 h-9 px-3"></input>
-                <div class="flex gap-3 mt-3 place-content-center">
-                    <button type="button" onClick={() => navigate('/Signup')} class="text-black cursor-pointer font-bold rounded-full bg-yellow-300 px-4 h-9 w-35 hover:scale-105 delay-120 duration-250 ease-in-out">Sign Up</button>
-                    <button type="submit" class="text-black cursor-pointer font-bold rounded-full bg-green-400 px-6 h-9 w-35 hover:scale-105 delay-120 duration-250 ease-in-out">Login</button>
-                </div>
+        <div className="flex place-content-center mt-5">
+            <form className="flex flex-col gap-2" action="" >
+                <label htmlFor="email" className="text-white text-2xl">Email</label>
+                <input type="email" 
+                  placeholder="Enter Email" className="bg-gray-300 rounded-full w-80 h-9 px-3"/>
+
+                <label htmlFor="password" className="text-white text-2xl">Password</label>
+                <input type="password" 
+                  placeholder="Enter Password" className="bg-gray-300 rounded-full w-80 h-9 px-3"/>
+
+                  <div className="flex place-content-center mt-4 gap-2">
+                    <button className="btn btn-success w-100 rounded-full">Sign in</button>
+                  </div>
+                  <div className="flex place-content-center mt-4 gap-2">
+                    <Link to="/Login" className="btn btn-success w-100 rounded-full text-decoration-none">
+                      Log in
+                    </Link>
+                  </div>
 
             </form>
         </div>
--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -2,15 +2,13 @@ import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom'
 import logo from '../assets/mapua_logo.svg'
-
+import { Link } from 'react-router-dom'
 
 function Signup() {
     const navigate = useNavigate();
     const [formValues, setFormValues] = useState({
-        firstname: '',
-        lastname: '',
         username: '',
-        employeenumber: '',
+        email: '',
         password: '',
     });
     
@@ -21,107 +19,62 @@ function Signup() {
 
     const handleSubmit=async (e)=>{
         e.preventDefault();
-        console.log(formValues);
-
-        try {
-          const response = await axios.post("http://localhost:3000/api/auth/register-user", formValues);
-          console.log(response, 'res');
-    
-          if (response.data.success) {
-
-              setFormValues({username:"",email:"",mobile:"",password:""});
-
-          } else {
-
-          }
-      } catch (error) {
-          console.error('Error during registration:', error);
-
-      }
-        
-        
+        console.log(formValues);        
     
       }
 
-    return (
-      <div class="flex flex-col h-screen bg-gray-950 place-content-center">
-        <div class="flex flex-col place-items-center text-center relative h-screen">
-            <div class="flex z-10 flex-col gap-8 mt-35">
-                <div class="flex z-10 w-98 h-15 rounded-[0.9vw] bg-red-600 text-white place-content-center py-2 text-4xl">
-                    <h2 class="text-white">Sign Up</h2>
-                </div>
-                <div class="flex bg-gray-700 z-10 p-6 rounded-[0.9vw] w-98">
-                    <form class="flex flex-col gap-2" onSubmit={handleSubmit}>
-                        <label class="text-white text-2xl self-start px-2">First Name</label>
-                        <input 
-                            type="text" 
-                            class="bg-gray-300 rounded-full w-80 h-9 px-3"
-                            placeholder="Enter your first name"
-                            name="firstname"
-                            value={formValues.firstname} 
-                            onChange={handleInputChange}
-                        
-                        />
-
-                        <label class="text-white text-2xl self-start px-2">Last Name</label>
-                        <input 
-                            type="text" 
-                            class="bg-gray-300 rounded-full w-80 h-9 px-3"
-                            placeholder="Enter your last name"
-                            name="lastname"
-                            value={formValues.lastname} 
-                            onChange={handleInputChange}
-                            />
-
-                        <label class="text-white text-2xl self-start px-2">Username</label>
-                        <input 
-                            type="text" 
-                            class="bg-gray-300 rounded-full w-80 h-9 px-3"
-                            placeholder="Username"
-                            name="username"
-                            value={formValues.username} 
-                            onChange={handleInputChange}                                
-                            />
-                            
-                        <label class="text-white text-2xl self-start px-2">Employee No.</label>
-                        <input 
-                            type="text" 
-                            class="bg-gray-300 rounded-full w-80 h-9 px-3"
-                            placeholder="Enter your Employee Number"
-                            name="employeenumber"
-                            value={formValues.employeenumber} 
-                            onChange={handleInputChange}    
-                            />
-
-                        <label class="text-white text-2xl self-start px-2">Password</label>
-                        <input 
-                            type="password" 
-                            class="bg-gray-300 rounded-full w-80 h-9 px-3"
-                            placeholder="Password"
-                            name="password"
-                            value={formValues.password} 
-                            onChange={handleInputChange}     
-                            />
-
-                        <div class="flex place-content-center mt-4 gap-2">
-                            <button type="submit" 
-                                onClick={() => navigate('/Login')} 
-                                class="text-black cursor-pointer font-bold rounded-full bg-yellow-300 px-6 h-9 w-35 hover:scale-105 delay-120 duration-250 ease-in-out">Back</button>
-                            
-                            <button type="submit" 
-                                class="text-black cursor-pointer font-bold rounded-full bg-green-400 px-6 h-9 w-35 hover:scale-105 delay-120 duration-250 ease-in-out">
-                                    Sign Up
-                            </button>
-                        </div>
-
-                    </form>
-                </div>
+      return (
+        <div className="flex flex-col bg-gray-950 place-content-center">
+          <div className="flex flex-col place-items-center text-center relative h-screen">
+            <div className="flex z-10 flex-col gap-8 mt-35">
+              <div className="flex z-10 w-98 h-15 rounded-[0.9vw] bg-red-600 text-white place-content-center py-2 text-4xl">
+                <h2 className="text-white">Sign Up</h2>
+              </div>
+              <div className="flex bg-gray-700 z-10 p-6 rounded-[0.9vw] w-98">
+                <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+                  <label className="text-white text-2xl self-start px-2">Username</label>
+                  <input
+                    type="text"
+                    className="bg-gray-300 rounded-full w-80 h-9 px-3"
+                    placeholder="Username"
+                    name="username"
+                    value={formValues.username}
+                    onChange={handleInputChange}
+                  />
+                  <label className="text-white text-2xl self-start px-2">E-mail</label>
+                  <input
+                    type="text"
+                    className="bg-gray-300 rounded-full w-80 h-9 px-3"
+                    placeholder="Enter your email"
+                    name="email"
+                    value={formValues.email}
+                    onChange={handleInputChange}
+                  />
+                  <label className="text-white text-2xl self-start px-2">Password</label>
+                  <input
+                    type="password"
+                    className="bg-gray-300 rounded-full w-80 h-9 px-3"
+                    placeholder="Password"
+                    name="password"
+                    value={formValues.password}
+                    onChange={handleInputChange}
+                  />
+                  <div className="flex place-content-center mt-4 gap-2">
+                    <button className="btn btn-success w-100 rounded-full">Sign in</button>
+                  </div>
+                  <div className="flex place-content-center mt-4 gap-2">
+                    <Link to="/Login" className="btn btn-success w-100 rounded-full text-decoration-none">
+                      Log in
+                    </Link>
+                  </div>
+                </form>
+              </div>
             </div>
-                <img src={logo} class="flex h-screen w-screen opacity-25 absolute z-0" />
+            <img src={logo} className="flex h-screen w-screen opacity-25 absolute z-0" />
+          </div>
         </div>
-
-      </div>
-    )
+      );
+      
 }
 
 
